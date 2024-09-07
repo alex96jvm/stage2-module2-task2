7201fb1948b1f4edb3ce9f99873fad57d4cc229a
alex96jvm <alex96jvm@gmail.com>
2024-09-07 15:49:39 +0300

    Complete task 2 Servlets

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,41 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("userId") == null) {
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+        else {
+            request.getRequestDispatcher("/user/hello.jsp").forward(request, response);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        List<String> users = Users.getInstance().getUsers();
+
+        if (users.contains(login) && !password.isEmpty()) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", login);
+            response.sendRedirect(request.getContextPath() + "/user/hello.jsp");
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+    }
 }
